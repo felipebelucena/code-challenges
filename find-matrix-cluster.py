d62c6cee8f18d5ed18efc9722116ee84217355d2
Felipe Lucena <felipelucena22@gmail.com>
2020-01-03 11:20:15 -0300

    Fix find-matrix-cluster algorithm. Add test cases

--- find-matrix-cluster.py
+++ find-matrix-cluster.py
@@ -1,33 +1,32 @@
-def get_adjacent_positions(currentPosition, maxPosition):
+def get_adjacents(currentPosition, maxPosition):
   row, column = currentPosition
   maxRow, maxColumn = maxPosition
   adj_positions = []
   
   if row > 0:
       adj_positions.append((row - 1, column))
-  if row < maxRow - 1:
+  if row < maxRow:
       adj_positions.append((row + 1, column))
   if column > 0:
       adj_positions.append((row, column - 1))
-  if column < maxColumn - 1:
+  if column < maxColumn:
       adj_positions.append((row, column + 1))
   
   return adj_positions
 
 def find_clusters(graph):
-  visited = { position: False for position in graph.keys()}
+  visited = {}
   clusters = 0
   
   for pos in graph.keys():
-    print(pos, " ", visited[pos])
-    if not visited[pos]:
+    if pos not in visited:
       clusters += 1
       visited[pos] = True
       queue = [pos]
       while len(queue):
         node = queue.pop(0) # dequeue
         for edge in graph[node]:
-          if not visited[edge]:
+          if edge not in visited:
             visited[edge] = True
             queue.append(edge)
 
@@ -35,17 +34,30 @@ def find_clusters(graph):
 
 def number_of_clusters(rows, columns, grid):
   graph = {}
-  maxPosition = (rows, columns)
+  maxPos = (rows - 1, columns - 1)
   for row in range(rows):
     for column in range(columns):
-      position = (row, column)
-      adj_positions = get_adjacent_positions(position, maxPosition)
-      graph[position] = [(i, j) for i, j in adj_positions if grid[i][j]]
+      if grid[row][column]:
+        pos = (row, column)
+        graph[pos] = [(i, j) for i, j in get_adjacents(pos, maxPos) if grid[i][j]]
   
   return find_clusters(graph)
 
 if __name__ == '__main__':
-  # test case 1
+   # no clusters
+  assert number_of_clusters(3, 3, [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ]) == 0
+
+  # 1 cluster
+  assert number_of_clusters(3, 5, [
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+  ]) == 1
+
   assert number_of_clusters(5, 4, [
     [1, 1, 0, 0],
     [0, 0, 1, 0],
@@ -54,10 +66,39 @@ if __name__ == '__main__':
     [1, 1, 1, 1]
   ]) == 3
 
-  # # test case 1
-  # assert number_of_clusters(4, 4, [
-  #   [1, 0, 0, 0],
-  #   [0, 1, 0, 0],
-  #   [0, 0, 1, 0],
-  #   [0, 0, 0, 1],
-  # ]) == 4
+  assert number_of_clusters(4, 4, [
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1],
+  ]) == 4
+
+  assert number_of_clusters(5, 7, [
+    [1, 0, 0, 0, 1, 1, 1],
+    [0, 1, 0, 0, 1, 1, 1],
+    [0, 0, 1, 0, 1, 1, 1],
+    [0, 0, 0, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1, 1, 1],
+  ]) == 5
+
+  assert number_of_clusters(5, 5, [
+    [1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1],
+  ]) == 5
+
+  assert number_of_clusters(4, 6, [
+    [1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0],
+  ]) == 2
+
+  assert number_of_clusters(4, 6, [
+    [1, 0, 1, 0, 1, 0],
+    [1, 0, 1, 0, 1, 0],
+    [1, 0, 1, 0, 1, 0],
+    [1, 0, 1, 0, 1, 0],
+  ]) == 3
